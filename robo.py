3179b50dcb6003b8642395847b5e3040c3e6b602
AhmadxonGafurov <gafurov1060@gmail.com>
2024-10-22 13:38:30 +0500

    22.10.2024

--- robo.py
+++ robo.py
@@ -1,28 +1,28 @@
-def sonni_top():
-    print("O'ylayotgan soningizni 1 dan 1000 gacha o'ylang.")
-    input("O'ylaganingizdan keyin Enter tugmasini bosing...")
+# def sonni_top():
+#     print("O'ylayotgan soningizni 1 dan 1000 gacha o'ylang.")
+#     input("O'ylaganingizdan keyin Enter tugmasini bosing...")
 
-    past = 1
-    yuqori = 1000
-    urinishlar = 0
+#     past = 1
+#     yuqori = 1000
+#     urinishlar = 0
 
-    while past <= yuqori:
-        urinishlar += 1
-        tahmin = (past + yuqori) // 2
-        javob = input(f"Men o'ylayapman: {tahmin}. Bu son (=) to'g'ri, (+) katta yoki (-) kichik? ").lower()
+#     while past <= yuqori:
+#         urinishlar += 1
+#         tahmin = (past + yuqori) // 2
+#         javob = input(f"Men o'ylayapman: {tahmin}. Bu son (=) to'g'ri, (+) katta yoki (-) kichik? ").lower()
 
-        if javob == '=':
-            print(f"Men topdim! Siz o'ylagan son {tahmin}.")
-            print(f"Men {urinishlar} urinishda topdim.")
-            break
-        elif javob == '-':
-            yuqori = tahmin - 1
-        elif javob == '+':
-            past = tahmin + 1
-        else:
-            print("Iltimos, '=', '+', yoki '-' deb yozing.")
+#         if javob == '=':
+#             print(f"Men topdim! Siz o'ylagan son {tahmin}.")
+#             print(f"Men {urinishlar} urinishda topdim.")
+#             break
+#         elif javob == '-':
+#             yuqori = tahmin - 1
+#         elif javob == '+':
+#             past = tahmin + 1
+#         else:
+#             print("Iltimos, '=', '+', yoki '-' deb yozing.")
 
-sonni_top()
+# sonni_top()
 # from math import sqrt
 
 # def tub_sonni_top(son):
@@ -50,8 +50,18 @@ sonni_top()
 #     if tub:
 #         tub_sonlar.append(son)
 # print(tub_sonlar)   
-a=input("Ism kiriting ")
-if bool(a):
-    print("Rahmat")
-else:
-    print("Ism kiritmadiz")
+# a=input("Ism kiriting ")
+# if bool(a):
+#     print("Rahmat")
+# else:
+#     print("Ism kiritmadiz")
+# while True:
+#     son=int(input("Son kiriting: "))
+#     print(f"Siz kiritkan sonning 2-darajasi {son**2}ga teng")
+#     savol=input("Yana kiritasizmi yes/no : ").lower()
+#     if savol=="yes":
+#         continue
+#     elif  savol=="no":
+#         print("Dastur tugadi ")
+#         break
+#     else:print("Error")
